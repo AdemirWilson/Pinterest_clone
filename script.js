eeb44a7a22a2cce561cf3c7e53a94c93b1ecf28e
AdemirWilson <developer@ademirwilson.com>
2021-09-03 15:40:14 -0300

    main

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const grids = document.querySelectorAll('.grid')
-const headings = document.querySelectorAll('.headings .text2 .text')
+const headings = document.querySelectorAll('.heading .text2 .text')
 
 function enterScreen(index){
     const grid = grids[index];
@@ -9,20 +9,48 @@ function enterScreen(index){
     grid.classList.add('active');
 
     gridColumns.forEach(element=> {
-        element.classList.remove('animate-before');
+        element.classList.remove('animate-before', 'animate-after');
     })
+
+    heading.classList.remove('animate-before', 'animate-after');
 }
 
 function exitScreen(index, exitDelay){
+    const grid = grids[index];
+    const heading = headings[index];
+    const gridColumns = grid.querySelectorAll('.column');
 
+    gridColumns.forEach(element=> {
+        element.classList.add('animate-after');
+    })
+
+    heading.classList.add('animate-after');
+
+    setTimeout(() => {
+        grid.classList.remove('active');
+    }, exitDelay)
 }
 
-function setupAnimationCycle({ initialScreenIndex, timePerScreen, exitDelay }){
-    enterScreen(initialScreenIndex)
+function setupAnimationCycle({ timePerScreen, exitDelay }){
+    const cycleTime = timePerScreen + exitDelay;
+    let nextIndex = 0;
+
+    function nextCycle() {
+        const currentIndex = nextIndex;
+
+        enterScreen(currentIndex);
+
+        setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen);
+
+        nextIndex = nextIndex >= grids.length -1 ? 0 : nextIndex + 1;
+    }
+
+    nextCycle();
+
+    setInterval(()=> nextCycle(), cycleTime);
 }
 
 setupAnimationCycle({
-    initialScreenIndex: 0,
     timePerScreen: 2000,
     exitDelay: 200 * 7
 })
